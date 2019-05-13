782b60434c997e32805e963f9aa0cc25d6d4adde
LukasRos1 <48435119+LukasRos1@users.noreply.github.com>
2019-05-13 15:11:40 +0200

    Add files via upload

---
+++ RateAGame/src/main/java/Spark/User.java
@@ -0,0 +1,15 @@
+package Spark;
+
+public class User {
+	
+	private boolean loggedIn;
+
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
+	}
+
+}
